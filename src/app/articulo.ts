68443e5a731981744110d9265eb3d69fa24c61f9
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-13 13:51:47 +0100

    mock-art actualizado

--- src/app/articulo.ts
+++ src/app/articulo.ts
@@ -1,6 +1,6 @@
-export interface Hero {
+export interface Articulo {
     id: number;
     nombre: string;
-    tipo: CharacterData;
+    tipo: string;
 
   }
---
+++ src/app/mock-articulos.ts
@@ -0,0 +1,14 @@
+import { Articulo } from './articulo';
+
+export const Articulos: Articulo[] = [
+  { id: 1, nombre: 'Pera', tipo:'F' },
+  { id: 2, nombre: 'Manzana', tipo:'F' },
+  { id: 3, nombre: 'Piña', tipo:'F' },
+  { id: 4, nombre: 'Naranja', tipo:'F' },
+  { id: 5, nombre: 'Melocoton', tipo:'F' },
+  { id: 6, nombre: 'Lechuga', tipo:'V' },
+  { id: 7, nombre: 'Pepino', tipo:'V' },
+  { id: 8, nombre: 'Nabicol', tipo:'V' },
+  { id: 9, nombre: 'Tomate', tipo:'V' },
+  { id: 10, nombre: 'Patata', tipo:'V' },
+];
