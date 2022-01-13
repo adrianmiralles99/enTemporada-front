48af367b2839be6e1b8312f3c652b078dac73835
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-13 14:22:08 +0100

    aaa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductesComponent } from './productes/productes.component';
-<<<<<<< HEAD
 import { BarraComponent } from './barra/barra.component';
-=======
-import { MenuFooterComponent } from './menu-footer/menu-footer.component';
->>>>>>> fd3fad06fb403e68181f39c729b6dc0372abe780
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductesComponent,
-<<<<<<< HEAD
     BarraComponent
-=======
-    MenuFooterComponent
->>>>>>> fd3fad06fb403e68181f39c729b6dc0372abe780
   ],
   imports: [
     BrowserModule,
