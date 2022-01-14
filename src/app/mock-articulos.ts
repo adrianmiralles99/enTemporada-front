0623af045d95e39fe42ff7be755f0ba12700a6c5
Carlos <cdmarin@iesfuentesanluis.org>
2022-01-14 08:32:25 +0100

    a

--- src/app/mock-articulos.ts
+++ src/app/mock-articulos.ts
@@ -3,12 +3,12 @@ import { Articulo } from './articulo';
 export const ARTICULOS: Articulo[] = [
   { id: 1, nombre: 'Pera', tipo: 'F' },
   { id: 2, nombre: 'Manzana', tipo: 'F' },
+  { id: 6, nombre: 'Lechuga', tipo: 'V' },
   { id: 3, nombre: 'Piña', tipo: 'F' },
   { id: 4, nombre: 'Naranja', tipo: 'F' },
   { id: 5, nombre: 'Melocoton', tipo: 'F' },
   { id: 5, nombre: 'Fresa', tipo: 'F' },
   { id: 5, nombre: 'Aguacate', tipo: 'F' },
-  { id: 6, nombre: 'Lechuga', tipo: 'V' },
   { id: 7, nombre: 'Pepino', tipo: 'V' },
   { id: 8, nombre: 'Nabicol', tipo: 'V' },
   { id: 9, nombre: 'Tomate', tipo: 'V' },
