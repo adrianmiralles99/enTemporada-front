96136169415b81519b1c32a7747ec0cacba9a60d
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-14 08:33:27 +0100

    Frutas y Verduras

--- src/app/mock-articulos.ts
+++ src/app/mock-articulos.ts
@@ -1,6 +1,6 @@
 import { Articulo } from './articulo';
 
-export const ARTICULOS: Articulo[] = [
+export const FRUTAS: Articulo[] = [
   { id: 1, nombre: 'Pera', tipo: 'F' },
   { id: 2, nombre: 'Manzana', tipo: 'F' },
   { id: 3, nombre: 'Piña', tipo: 'F' },
@@ -8,6 +8,21 @@ export const ARTICULOS: Articulo[] = [
   { id: 5, nombre: 'Melocoton', tipo: 'F' },
   { id: 5, nombre: 'Fresa', tipo: 'F' },
   { id: 5, nombre: 'Aguacate', tipo: 'F' },
+  { id: 6, nombre: 'Cereza', tipo: 'F' },
+  { id: 7, nombre: 'Ciruela', tipo: 'F' },
+  { id: 8, nombre: 'Nispero', tipo: 'F' },
+  { id: 9, nombre: 'Limon', tipo: 'F' },
+  { id: 10, nombre: 'Platano', tipo: 'F' },
+  { id: 10, nombre: 'Pomelo', tipo: 'F' },
+];
+export const VERDURAS: Articulo[] = [
+  { id: 1, nombre: 'Batata', tipo: 'V' },
+  { id: 2, nombre: 'Coloflor', tipo: 'V' },
+  { id: 3, nombre: 'Nabo', tipo: 'V' },
+  { id: 4, nombre: 'Alcachofa', tipo: 'V' },
+  { id: 5, nombre: 'Col', tipo: 'V' },
+  { id: 5, nombre: 'Lecuga', tipo: 'V' },
+  { id: 5, nombre: 'Aguacate', tipo: 'V' },
   { id: 6, nombre: 'Lechuga', tipo: 'V' },
   { id: 7, nombre: 'Pepino', tipo: 'V' },
   { id: 8, nombre: 'Nabicol', tipo: 'V' },
