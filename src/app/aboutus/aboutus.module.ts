5fd696c09f0dcb2b7d2cce7c588e0e8b4f973e32
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-17 09:45:46 +0100

    a

--- src/app/aboutus/aboutus.module.ts
+++ src/app/aboutus/aboutus.module.ts
@@ -1,34 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { IndexComponent } from './index/index.component';
-=======
 import { PruebaComponent } from './prueba/prueba.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
->>>>>>> b3c569f4b7ba7baf1ce4c15a1c78306b5130cfca
 
 
 
 @NgModule({
   declarations: [
   
-<<<<<<< HEAD
-    IndexComponent
-=======
     PruebaComponent,
        PresentacionComponent
->>>>>>> b3c569f4b7ba7baf1ce4c15a1c78306b5130cfca
   ],
   imports: [
     CommonModule
   ]
   , exports: [
-<<<<<<< HEAD
-    IndexComponent
-=======
     PruebaComponent,
     PresentacionComponent
->>>>>>> b3c569f4b7ba7baf1ce4c15a1c78306b5130cfca
   ]
 })
 export class AboutusModule { }
