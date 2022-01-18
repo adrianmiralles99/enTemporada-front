02e120c0e53bbc9f9369f862bc0c8b223662b7ec
amiralles2 <amiralles@iesfuentesanluis.org>
2022-01-18 12:20:23 +0100

    sqwswdwdwqd

--- src/app/registro/registro.module.ts
+++ src/app/registro/registro.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UtilsModule } from '../utils/utils.module';
 import { IndexregComponent } from './indexreg/indexreg.component';
+import { DatosloginComponent } from './datoslogin/datoslogin.component';
 
 
 
 @NgModule({
-  declarations: [IndexregComponent],
+  declarations: [IndexregComponent, DatosloginComponent],
   imports: [
     CommonModule,
     UtilsModule
