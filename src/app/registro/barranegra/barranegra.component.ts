80e050b880d826fd25ff647c5ccf72e878cf6959
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-18 12:20:19 +0100

    a

---
+++ src/app/registro/barranegra/barranegra.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barranegra',
+  templateUrl: './barranegra.component.html',
+  styleUrls: ['./barranegra.component.scss']
+})
+export class BarranegraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
