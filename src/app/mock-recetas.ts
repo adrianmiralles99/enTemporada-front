05552de84e2acefa0d0d5ceb0f8a0ddb56553062
Carlos <cdmarin@iesfuentesanluis.org>
2022-01-20 22:29:24 +0100

    recetas

--- src/app/mock-recetas.ts
+++ src/app/mock-recetas.ts
@@ -1,9 +1,10 @@
 import { Receta } from '../app/receta';
 
 export const RECETAS: Receta[] = [
-    { id: 1, titulo: 'Tarta de Manzana', autor: 'alturano' },
-    { id: 2, titulo: 'Humus de Aguacate', autor: 'adrimigo' },
-    { id: 3, titulo: 'Boniatos asados rellenos', autor: 'ByCharles' },
-    { id: 4, titulo: 'Sopa de remolacha y yogur', autor: 'el_vikingo' },
-    { id: 5, titulo: 'Ensalada de calabaza asada', autor: 'el_vikingo' },
+    { id: 1, titulo: 'Tarta de Manzana', autor: 'alturano', img: "tartaManzana" },
+    { id: 2, titulo: 'Hummus de Aguacate', autor: 'adrimigo', img: "hummus" },
+    { id: 3, titulo: 'Boniatos asados rellenos', autor: 'ByCharles', img: "boniatos" },
+    { id: 4, titulo: 'Sopa de remolacha y yogur', autor: 'el_vikingo', img: "sopaRemolacha" },
+    { id: 5, titulo: 'Ensalada de calabaza asada', autor: 'Juan Monta Puercos', img: "ensalada" },
+    { id: 6, titulo: 'Banana Split', autor: 'Jeni Electrica', img: "banana" },
 ];
