e24d3588fce00d1b134f900aab5c5ff106b65829
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-01-20 11:30:48 +0100

    a

--- src/app/infoproducto/infoproducto.module.ts
+++ src/app/infoproducto/infoproducto.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DatosproductoComponent } from './datosproducto/datosproducto.component';
-import { BarranegraComponent } from '../utils/barranegra/barranegra.component';
 import { IndexprodComponent } from './indexprod/indexprod.component';
 import { UtilsModule } from '../utils/utils.module';
 import { DescripcionComponent } from './descripcion/descripcion.component';
