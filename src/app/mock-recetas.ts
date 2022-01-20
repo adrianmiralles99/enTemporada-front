7b7ccc768f691cfc3d0a84b14ee3aa22237f8a92
Carlos <cdmarin@iesfuentesanluis.org>
2022-01-20 11:39:24 +0100

    arreglo de barra superior

--- src/app/mock-recetas.ts
+++ src/app/mock-recetas.ts
@@ -1,9 +1,9 @@
 import { Receta } from '../app/receta';
 
 export const RECETAS: Receta[] = [
-    { id: 1, titulo: 'Tarta de Manzana', autor: 'alturano'},
-    { id: 2, titulo: 'Humus de Aguacate', autor: 'adrimigo'},
-    { id: 3, titulo: 'Boniatos asados rellenos', autor: 'ByCharles'},
-    { id: 4, titulo: 'Sopa de remoalcha y yogur', autor: 'el_vikingo'},
-    { id: 5, titulo: 'Ensalada de calabaza asada', autor: 'el_vikingo'},
+    { id: 1, titulo: 'Tarta de Manzana', autor: 'alturano' },
+    { id: 2, titulo: 'Humus de Aguacate', autor: 'adrimigo' },
+    { id: 3, titulo: 'Boniatos asados rellenos', autor: 'ByCharles' },
+    { id: 4, titulo: 'Sopa de remolacha y yogur', autor: 'el_vikingo' },
+    { id: 5, titulo: 'Ensalada de calabaza asada', autor: 'el_vikingo' },
 ];
