bc1f9c5b2c0186d426bf9d286f5ac717b52a8e54
Carlos <cdmarin@iesfuentesanluis.org>
2022-01-21 10:40:12 +0100

    cojo

--- src/app/creacion-receta/datoscreacion/datoscreacion.component.ts
+++ src/app/creacion-receta/datoscreacion/datoscreacion.component.ts
@@ -12,6 +12,8 @@ export class DatoscreacionComponent implements OnInit {
   ingredientes = ["4 Manzanas", "300g de harina", "3 Peras", "4 vasos de agua", "4 maracatones", "Macarrones con queso"]
 
   ngOnInit(): void {
+
   }
+
 
 }
