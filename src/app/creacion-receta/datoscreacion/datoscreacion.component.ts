948891a9d06f8981e82b81cdf907677f443b4329
Carlos <cdmarin@iesfuentesanluis.org>
2022-01-21 12:25:38 +0100

    penoso

--- src/app/creacion-receta/datoscreacion/datoscreacion.component.ts
+++ src/app/creacion-receta/datoscreacion/datoscreacion.component.ts
@@ -9,9 +9,23 @@ export class DatoscreacionComponent implements OnInit {
 
   constructor() { }
 
-  ingredientes = ["4 Manzanas", "300g de harina", "3 Peras", "4 vasos de agua", "4 maracatones", "Macarrones con queso"]
+  // INGREDIENTES SE COGERÁ DE LA BASE DE DATOS, ESTA VARIABLE NO EXISTIRÁ
+  ingredientes = ["4 Manzanas grandes", "2 huevos M", "250 ml. de leche entera", "250 g. de azúcar ", "120 g. de harina de trigo de todo uso", "1 plancha de masa quebrada",
+    "50 g. de mermelada de melocotón o albaricoque", "1 cda. de agua", "Molde desmoldable 18cm"];
+
+  ingredientes1 = [""];
+  ingredientes2 = [""];
 
   ngOnInit(): void {
+    if (this.ingredientes.length % 2 == 0) {
+      this.ingredientes1 = this.ingredientes.splice(0, (this.ingredientes.length / 2));
+    }
+    else {
+      this.ingredientes1 = this.ingredientes.splice(0, (this.ingredientes.length / 2) + 1);
+    }
+
+    this.ingredientes2 = this.ingredientes.splice(0, this.ingredientes.length);
+
 
   }
 
