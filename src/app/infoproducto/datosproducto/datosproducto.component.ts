cc7d6f445452a86acbe44419462567e7a8235e5e
Carlos <cdmarin@iesfuentesanluis.org>
2022-02-07 08:11:08 +0100

    c

--- src/app/infoproducto/datosproducto/datosproducto.component.ts
+++ src/app/infoproducto/datosproducto/datosproducto.component.ts
@@ -1,8 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Articulo } from 'src/app/articulo';
 import { Productos } from 'src/app/modelos/productos.model';
-import { ProductosService } from 'src/app/servicios/productos.service';
 
 @Component({
   selector: 'app-datosproducto',
@@ -12,28 +9,12 @@ import { ProductosService } from 'src/app/servicios/productos.service';
 })
 
 export class DatosproductoComponent implements OnInit {
-  constructor(private rutaActiva: ActivatedRoute,private productosService: ProductosService) { }
+  constructor() { }
   @Input() articulo!: Productos;
+
   meses = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
-  id = this.rutaActiva.snapshot.paramMap.get('id');
-  help = new Date();
-  mesActual = this.help.getMonth();
-  info =  [];
+  mesActual = new Date().getMonth();
 
-  getIndividual(): void {
-    this.productosService.getById(this.id)
-    .subscribe({
-      next: (data) =>{
-        this.articulo = data;
-        console.log(this.articulo.info_nut?.e);
-        
-      },
-      error: (e) => console.error(e)
-    })
-  }
-
-  ngOnInit(): void { 
-    this.getIndividual()
-  }
+  ngOnInit(): void { }
 
 }
--- src/app/infoproducto/indexprod/indexprod.component.ts
+++ src/app/infoproducto/indexprod/indexprod.component.ts
@@ -3,21 +3,30 @@ import { ActivatedRoute } from '@angular/router';
 import { Productos } from 'src/app/modelos/productos.model';
 import { ProductosService } from 'src/app/servicios/productos.service';
 
-
 @Component({
   selector: 'app-indexprod',
   templateUrl: './indexprod.component.html',
   styleUrls: ['./indexprod.component.scss']
 })
 export class IndexprodComponent implements OnInit {
-  constructor(private rutaActiva: ActivatedRoute) { }
-  
-  ngOnInit(): void { 
-   
+  constructor(private productosService: ProductosService, private rutaActiva: ActivatedRoute) { }
+
+  articulo!: Productos;
+
+  getIndividual(): void {
+    this.productosService.getById(this.rutaActiva.snapshot.paramMap.get('id'))
+      .subscribe({
+        next: (data) => {
+          this.articulo = data;
+        },
+        error: (e) => console.error(e)
+      })
   }
 
+  ngOnInit(): void {
+    this.getIndividual();
+  }
 
-  id = this.rutaActiva.snapshot.paramMap.get('id');
 
 
 
