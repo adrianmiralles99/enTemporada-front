94f03e3cff0bf89db0476b7a60ff6c37cd2bcf6a
pmartin011 <pmartin@iiesfuentesanluis.org>
2022-02-08 09:48:05 +0100

    :)

--- src/app/baseurl.ts
+++ src/app/baseurl.ts
@@ -1,2 +1,3 @@
-// export const serverUrl = 'http://alum3.iesfsl.org/EnTemporada/EnTemporada_restapi/web/';
-export const serverUrl = 'http://localhost/EnTemporada_restapi/web/';
+//export const serverUrl = 'http://alum3.iesfsl.org/EnTemporada/EnTemporada_restapi/web/';
+//Carlos    export const serverUrl = 'http://localhost/EnTemporada_restapi/web/';
+export const serverUrl = 'http://localhost/restapi/EnTemporada_restapi/web/';
--- src/app/calendario/tabla-calendario/tabla-calendario.component.ts
+++ src/app/calendario/tabla-calendario/tabla-calendario.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { PRODUCTES } from '../../mock-articulos';
+import { Calendario } from 'src/app/modelos/calendario.model';
+import { Productos } from 'src/app/modelos/productos.model';
+import { CalendarioService } from 'src/app/servicios/calendario.service';
+import { ProductosService } from 'src/app/servicios/productos.service';
 @Component({
   selector: 'app-tabla-calendario',
   templateUrl: './tabla-calendario.component.html',
@@ -7,22 +10,46 @@ import { PRODUCTES } from '../../mock-articulos';
 })
 export class TablaCalendarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productosService: ProductosService, private calendarioService: CalendarioService) { }
   meses = ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
-  frutas = PRODUCTES.filter(element => element.tipo == "F");
-  verduras = PRODUCTES.filter(element => element.tipo == "V");
-  productes = PRODUCTES;
+  productes: Productos[] = [];
+  frutas: Productos[] = [];
+  verduras: Productos[] = [];
+  calendario: Calendario[] = []
   help = new Date();
   mesActual = this.help.getMonth();
   mesact = this.meses[this.mesActual];
   messig = this.meses[this.mesActual + 1];
 
 
-
-
-
-
   ngOnInit(): void {
+    this.getProductes();
+    this.getCalendario();
+  }
+
+  getProductes(): void {
+    this.productosService.getCard()
+      .subscribe({
+        next: (data) => {
+          this.productes = data;
+          this.frutas = this.productes.filter(element => element.tipo == "F");
+          this.verduras = this.productes.filter(element => element.tipo == "V");
+          console.log("Productos");
+          
+        },
+        error: (e) => console.error(e)
+      });
+  }
+  getCalendario(): void {
+    this.calendarioService.getConProductos()
+    .subscribe({
+      next: (data) => {
+      this.calendario = data;
+        console.log("Calendarios");
+        
+      },
+      error: (e) => console.error(e)
+    });
   }
 
   cambiaColor(id: number, letra: string) {
